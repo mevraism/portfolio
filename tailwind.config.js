a7caf647a7687e0e6a1ad0aa2a78acfbb403b830
Ernestiyo Guevara <ernestiyoguevara@gmail.com>
2023-09-30 02:43:49 +0700

    'portfoli

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#06b6d4",
+        dark: "#083344",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
